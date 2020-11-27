cf5614b855198922c59162140af6c35dd3327fb4
Sergio Cortizo De Dios <sergio.cortizo.dedios@udc.es>
2020-11-27 18:29:14 +0100

    Añadidos test basados en propiedades para la clase Log

---
+++ src/test/java/gal/udc/fic/vvs/archivadorTest/LogPBT.java
@@ -0,0 +1,29 @@
+package gal.udc.fic.vvs.archivadorTest;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.runner.RunWith;
+
+import com.pholser.junit.quickcheck.From;
+import com.pholser.junit.quickcheck.Property;
+import com.pholser.junit.quickcheck.generator.Ctor;
+import com.pholser.junit.quickcheck.generator.InRange;
+import com.pholser.junit.quickcheck.runner.JUnitQuickcheck;
+
+import gal.udc.fic.vvs.email.archivador.ArchivadorSimple;
+import gal.udc.fic.vvs.email.archivador.Log;
+import gal.udc.fic.vvs.email.archivo.Texto;
+import gal.udc.fic.vvs.email.correo.Mensaje;
+
+@RunWith(JUnitQuickcheck.class)
+public class LogPBT {
+
+	@Property
+	public void logTest_almacenarCorreo(@InRange(min = "1") int espacioArchivador,
+										@From(Ctor.class) Texto texto) {
+		Log logTest = new Log(new ArchivadorSimple("archivadorPrueba", espacioArchivador));
+		Mensaje mensaje = new Mensaje(texto);
+		
+		assertTrue(logTest.almacenarCorreo(mensaje));
+	}
+}
