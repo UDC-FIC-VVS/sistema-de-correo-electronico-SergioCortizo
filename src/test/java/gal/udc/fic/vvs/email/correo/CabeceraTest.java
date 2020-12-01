2a8174d503eef8f74da365237d705ec21d72185f
Sergio Cortizo De Dios <sergio.cortizo.dedios@udc.es>
2020-12-01 19:50:08 +0100

    Ampliados los test unitarios de la clase Cabecera
    Ampliados los test unitarios de la clase Cabecera, cubriendo todos los métodos que implementa

--- src/test/java/gal/udc/fic/vvs/email/correo/CabeceraTest.java
+++ src/test/java/gal/udc/fic/vvs/email/correo/CabeceraTest.java
@@ -10,54 +10,175 @@ import gal.udc.fic.vvs.email.archivo.Texto;
 import gal.udc.fic.vvs.email.correo.Cabecera;
 import gal.udc.fic.vvs.email.correo.Mensaje;
 
+import java.util.Collection;
+import java.util.Vector;
+
 public class CabeceraTest {
 	@Rule
 	public ExpectedException thrown = ExpectedException.none();
-	
+
 	private String nombre = "Prueba";
 	private String valor = "Valor de prueba";
 	private Mensaje mensaje = new Mensaje(new Texto("Mensaje de prueba", "Esto es un mensaje de prueba"));
-	
+	private Carpeta carpeta = new Carpeta(nombre);
+
 	@Test
 	public void cabeceraIsWellFormedTest() {
 		Cabecera cabeceraDePrueba = new Cabecera(mensaje, nombre, valor);
-		
-		assertEquals(mensaje.obtenerTamaño() + nombre.length() + valor.length(),
-					cabeceraDePrueba.obtenerTamaño());
+
+		assertEquals(mensaje.obtenerTamaño() + nombre.length() + valor.length(), cabeceraDePrueba.obtenerTamaño());
 		assertEquals(nombre + ": " + valor + "\n" + mensaje.obtenerVisualizacion(),
-					cabeceraDePrueba.obtenerVisualizacion());
-		
+				cabeceraDePrueba.obtenerVisualizacion());
+
 	}
-	
-	//@Test
+
+	// @Test
 	public void cabeceraNoAñadidaSiNombreYValorSonVaciosTest() {
 		Cabecera cabeceraDePrueba = new Cabecera(mensaje, "", "");
-		
-		//TODO: No tiene sentido añadir cabeceras vacías
-		assertEquals(mensaje.obtenerTamaño(),
-				cabeceraDePrueba.obtenerTamaño());
-		assertEquals(mensaje.obtenerVisualizacion(),
-				cabeceraDePrueba.obtenerVisualizacion());
+
+		// TODO: No tiene sentido añadir cabeceras vacías
+		assertEquals(mensaje.obtenerTamaño(), cabeceraDePrueba.obtenerTamaño());
+		assertEquals(mensaje.obtenerVisualizacion(), cabeceraDePrueba.obtenerVisualizacion());
 	}
-	
+
 	@Test
 	public void cabeceraNoAñadidaSiValorEsNullTest() {
 		Cabecera cabeceraDePrueba = new Cabecera(mensaje, nombre, null);
 		thrown.expect(NullPointerException.class);
 		cabeceraDePrueba.obtenerTamaño();
 	}
-	
+
 	@Test
 	public void cabeceraNoAñadidaSiNombreEsNullTest() {
 		Cabecera cabeceraDePrueba = new Cabecera(mensaje, null, valor);
 		thrown.expect(NullPointerException.class);
 		cabeceraDePrueba.obtenerTamaño();
 	}
-	
+
 	@Test
 	public void cabeceraNoAñadidaSiMensajeEsNullTest() {
 		Cabecera cabeceraDePrueba = new Cabecera(null, nombre, valor);
 		thrown.expect(NullPointerException.class);
 		cabeceraDePrueba.obtenerTamaño();
+	}
+
+	@Test
+	public void obtenerNoLeidosConMensaje() {
+		Cabecera cabeceraDePrueba = new Cabecera(mensaje, nombre, valor);
+
+		assertEquals(cabeceraDePrueba.obtenerNoLeidos(), 1);
+	}
+
+	@Test
+	public void establecerLeidoConMensaje() {
+		Cabecera cabeceraDePrueba = new Cabecera(mensaje, nombre, valor);
+
+		cabeceraDePrueba.establecerLeido(true);
+
+		assertEquals(cabeceraDePrueba.obtenerNoLeidos(), 0);
+	}
+
+	@Test
+	public void obtenerIconoConMensajeNoLeido() {
+		Cabecera cabeceraDePrueba = new Cabecera(mensaje, nombre, valor);
+
+		assertEquals(cabeceraDePrueba.obtenerIcono(), Correo.ICONO_NUEVO_MENSAJE);
+	}
+
+	@Test
+	public void obtenerIconoConMensajeLeido() {
+		Cabecera cabeceraDePrueba = new Cabecera(mensaje, nombre, valor);
+
+		cabeceraDePrueba.establecerLeido(true);
+
+		assertEquals(cabeceraDePrueba.obtenerIcono(), Correo.ICONO_MENSAJE);
+	}
+
+	@Test
+	public void obtenerPreVisualizacion() {
+		Cabecera cabeceraDePrueba = new Cabecera(mensaje, nombre, valor);
+
+		assertEquals(cabeceraDePrueba.obtenerPreVisualizacion(), mensaje.obtenerPreVisualizacion());
+	}
+
+	@Test
+	public void obtenerRutaSinCarpetaPadre() {
+		Cabecera cabeceraDePrueba = new Cabecera(mensaje, nombre, valor);
+
+		assertEquals(cabeceraDePrueba.obtenerRuta(), mensaje.obtenerRuta());
+	}
+
+	@Test
+	public void obtenerRutaConCarpetaPadre() {
+		Cabecera cabeceraDePrueba = new Cabecera(mensaje, nombre, valor);
+		Carpeta carpeta = new Carpeta("Carpeta de prueba");
+
+		cabeceraDePrueba.establecerPadre(carpeta);
+
+		assertEquals(cabeceraDePrueba.obtenerRuta(), mensaje.obtenerRuta());
+	}
+	
+	@Test
+	public void explorarTestOperacionInvalidaExceptionExpected() throws OperacionInvalida {
+		Cabecera cabeceraDePrueba = new Cabecera(mensaje, nombre, valor);
+
+		thrown.expect(OperacionInvalida.class);
+		
+		cabeceraDePrueba.explorar();
+	}
+	
+	@Test
+	public void añadirTestOperacionInvalidaExceptionExpected() throws OperacionInvalida {
+		Cabecera cabeceraDePrueba = new Cabecera(mensaje, nombre, valor);
+
+		thrown.expect(OperacionInvalida.class);
+		
+		cabeceraDePrueba.añadir(mensaje);
+	}
+	
+	@Test
+	public void eliminarTestOperacionInvalidaExceptionExpected() throws OperacionInvalida {
+		Cabecera cabeceraDePrueba = new Cabecera(mensaje, nombre, valor);
+
+		thrown.expect(OperacionInvalida.class);
+		
+		cabeceraDePrueba.eliminar(mensaje);
+	}
+	
+	@Test
+	public void obtenerHijoTestOperacionInvalidaExceptionExpected() throws OperacionInvalida {
+		Cabecera cabeceraDePrueba = new Cabecera(mensaje, nombre, valor);
+
+		thrown.expect(OperacionInvalida.class);
+		
+		cabeceraDePrueba.obtenerHijo(0);
+	}
+	
+	@Test
+	public void obtenerPadreSinCarpetaPadre() {
+		Cabecera cabeceraDePrueba = new Cabecera(mensaje, nombre, valor);
+
+		assertEquals(null, cabeceraDePrueba.obtenerPadre());
+	}
+	
+	@Test
+	public void obtenerPadreConCarpetaPadre() {
+		Cabecera cabeceraDePrueba = new Cabecera(mensaje, nombre, valor);
+
+		cabeceraDePrueba.establecerPadre(carpeta);
+		
+		assertEquals(carpeta, cabeceraDePrueba.obtenerPadre());
+	}
+	
+	@Test
+	public void buscarConExito() {
+		Cabecera cabeceraDePrueba = new Cabecera(mensaje, nombre, valor);
+
+		Collection collectionExpected = new Vector();
+		collectionExpected.add(cabeceraDePrueba);
+		
+		Collection collectionObtained = cabeceraDePrueba.buscar("prueba");
+		
+		assertEquals(collectionObtained, collectionExpected);
 	}
 }
