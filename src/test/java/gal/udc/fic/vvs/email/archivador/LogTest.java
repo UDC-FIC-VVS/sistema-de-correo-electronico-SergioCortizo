a739399d2d0af59de4658dc8e321586d24b559ff
Sergio Cortizo De Dios <sergio.cortizo.dedios@udc.es>
2020-12-20 13:50:57 +0100

    Ampliados test unitarios de la clase Log para mejorar res. mutaciones

--- src/test/java/gal/udc/fic/vvs/email/archivador/LogTest.java
+++ src/test/java/gal/udc/fic/vvs/email/archivador/LogTest.java
@@ -1,12 +1,18 @@
 package gal.udc.fic.vvs.email.archivador;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
 import gal.udc.fic.vvs.email.archivador.ArchivadorSimple;
 import gal.udc.fic.vvs.email.archivador.Delegado;
 import gal.udc.fic.vvs.email.archivador.Log;
+import gal.udc.fic.vvs.email.archivo.Texto;
+import gal.udc.fic.vvs.email.correo.Carpeta;
+import gal.udc.fic.vvs.email.correo.Mensaje;
+import gal.udc.fic.vvs.email.correo.OperacionInvalida;
 
 public class LogTest {
 	
@@ -48,6 +54,38 @@ public class LogTest {
 		
 		assertEquals(null, log.obtenerDelegado());
 	}
+
+	@Test
+	public void LogTest_almacenarCorreo () {
+		ArchivadorSimple archivador = new ArchivadorSimple("archivadorPrueba", 1);
+		
+		ArchivadorSimple archivadorDelegado = new ArchivadorSimple("archivadorDelegado", 1);
+		
+		Log log = new Log(archivador);
+		
+		log.establecerDelegado(archivadorDelegado);
+		
+		boolean result = log.almacenarCorreo(new Carpeta("carpeta"));
+		
+		assertTrue(result);
+	}
 	
-	
+	@Test
+	public void LogTest_almacenarCorreo_false () throws OperacionInvalida {
+		ArchivadorSimple archivador = new ArchivadorSimple("archivadorPrueba", 1);
+		
+		ArchivadorSimple archivadorDelegado = new ArchivadorSimple("archivadorDelegado", 1);
+		
+		Log log = new Log(archivador);
+		
+		log.establecerDelegado(archivadorDelegado);
+		
+		Carpeta carpeta = new Carpeta("carpeta");
+		
+		carpeta.añadir(new Mensaje(new Texto("mensaje", "mensaje")));
+		
+		boolean result = log.almacenarCorreo(carpeta);
+		
+		assertFalse(result);
+	}
 }
