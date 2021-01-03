b09adc4a0cd32beea982c3c8e18a60a7c34c1f24
Sergio Cortizo De Dios <sergio.cortizo.dedios@udc.es>
2021-01-03 17:10:14 +0100

    Añadidos test que usan la suite TestsSuite

--- src/test/java/gal/udc/fic/vvs/email/TestsSuite.java
+++ src/test/java/gal/udc/fic/vvs/email/TestsSuite.java
@@ -12,6 +12,10 @@ import etm.core.monitor.EtmMonitor;
 import etm.core.renderer.SimpleTextRenderer;
 import gal.udc.fic.vvs.email.archivador.ArchivadorSimplePBT;
 import gal.udc.fic.vvs.email.archivador.ArchivadorSimpleTest;
+import gal.udc.fic.vvs.email.archivador.DelegadoPBT;
+import gal.udc.fic.vvs.email.archivador.DelegadoTest;
+import gal.udc.fic.vvs.email.archivador.LogPBT;
+import gal.udc.fic.vvs.email.archivador.LogTest;
 import gal.udc.fic.vvs.email.archivo.AudioPBT;
 import gal.udc.fic.vvs.email.archivo.AudioTest;
 import gal.udc.fic.vvs.email.archivo.ImagenPBT;
@@ -28,7 +32,8 @@ import gal.udc.fic.vvs.email.archivo.TextoTest;
 @RunWith(Suite.class)
 @SuiteClasses({ImagenTest.class, TextoTest.class, AudioTest.class,
 	AudioPBT.class, ImagenPBT.class, TextoPBT.class, ArchivadorSimpleTest.class,
-	ArchivadorSimplePBT.class})
+	ArchivadorSimplePBT.class, DelegadoPBT.class, DelegadoTest.class,
+	LogPBT.class, LogTest.class})
 public class TestsSuite {
 
 	private static EtmMonitor monitor;
